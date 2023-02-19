2c09e47a9ecd89ba7b38438c9922493c88ae26d6
coin6442@gmail.com <coin6442@gmail.com>
2023-02-20 05:08:33 +0900

    fix : 커스텀 인증 등록 수정

--- todolist/settings.py
+++ todolist/settings.py
@@ -128,6 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'todolist.authentication.CustomAuthentication'
     ]
 }
