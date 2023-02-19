11c37ac203610df54fb9b25c5bec471daa25d7a0
coin6442@gmail.com <coin6442@gmail.com>
2023-02-20 04:20:45 +0900

    add : Plan 모델 생성

--- plan_app/models.py
+++ plan_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Plan(models.Model):
+    
+    """Plan Model Definition"""
+    
+    date = models.DateField()
+    content = models.TextField()
+    isChecked = models.BooleanField(default=False)
+    emoji = models.CharField(
+        max_length=1,
+        default="",
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content
