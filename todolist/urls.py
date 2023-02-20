6e1c29a5237a3250f6fd95d0dbdc489dd4e28022
coin6442@gmail.com <coin6442@gmail.com>
2023-02-20 12:23:55 +0900

    fix : url 오타 수정

--- todolist/urls.py
+++ todolist/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include("user_app.urls")),
-    path('api/plans', include("plan_app.urls")),
+    path('api/plans/', include("plan_app.urls")),
 ]
