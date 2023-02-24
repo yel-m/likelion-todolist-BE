dcf7b59fcb9e240a5e7f9c82b4854edcecbc85a6
coin6442@gmail.com <coin6442@gmail.com>
2023-02-25 02:05:50 +0900

    fix : 회원가입 url 수정

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Users.as_view()),
+    path("sign-up", views.Users.as_view()),
+    path("log-in", views.LogIn.as_view()),
 ]
