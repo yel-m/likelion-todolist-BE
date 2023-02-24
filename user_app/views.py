acbf772bb6cf79f512067d95ab2d53095bba0d6f
coin6442@gmail.com <coin6442@gmail.com>
2023-02-25 02:07:29 +0900

    add : 로그인

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError, NotFound
@@ -19,3 +20,17 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+class LogIn(APIView):
+    
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "유저 네임 또는 패스워드가 틀렸습니다."})
