326009046ca159524956f0d5a087cf1c8cb98537
yel-m <coin6442@gmail.com>
2023-05-28 09:35:37 +0900

    fix : 경로명 수정

--- plan_app/urls.py
+++ plan_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:username>", views.Plans.as_view()),
-    path("<str:username>/<int:id>", views.PlanDetail.as_view()),
-    path("<str:username>/<int:id>/check", views.PlanCheck.as_view()),
-    path("<str:username>/<int:id>/reviews", views.PlanReview.as_view()),
+    path("<int:user_id>", views.Plans.as_view()),
+    path("<int:user_id>/<int:plan_id>", views.PlanDetail.as_view()),
+    path("<int:user_id>/<int:plan_id>/check", views.PlanCheck.as_view()),
+    path("<int:user_id>/<int:plan_id>/reviews", views.PlanReview.as_view()),
 ]
