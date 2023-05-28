b3e465aa87c08bc36b258874e0f685cab610c308
yel-m <coin6442@gmail.com>
2023-05-28 09:41:08 +0900

    fix : Response 메시지 수정

--- user_app/views.py
+++ user_app/views.py
@@ -5,18 +5,14 @@ from . import serializers
 from .models import User
 
 class Users(APIView):
-
     # 유저 생성
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        if not username or not password:
-            raise ParseError
         serializer = serializers.UserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            serializer = serializers.UserSerializer(user)
-            return Response(serializer.data)
+            serializer.save()
+            return Response({
+                "message": "회원가입 요청이 성공적으로 처리되었습니다."
+            })
         else:
             return Response(serializer.errors)
         
@@ -26,21 +22,22 @@ class LogIn(APIView):
             user = User.objects.get(username=username, password=password)
             return user
         except User.DoesNotExist:
-            raise NotFound
+            raise NotFound("유저를 찾을 수 없습니다.")
         
     
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
         if not username or not password:
-            raise ParseError
+            raise ParseError("username 또는 password가 필요합니다.")
         user = self.get_user(username, password)
         return Response({
-                "detail": "로그인 성공",
-                "username": user.username
+            "user_id": user.id
         })
         
 class LogOut(APIView):
     
     def post(self, request):
-        return Response({"ok": "bye!"})
+        return Response({
+            "message": "로그아웃 요청이 성공적으로 처리되었습니다."
+        })
